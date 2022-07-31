3f243531d216287f6c99d95454bc7b9c2830135a
Flavio Ialongo <flavioialongo@gmail.com>
2022-08-01 01:11:13 +0200

    boh

--- src/Todo.js
+++ src/Todo.js
@@ -7,8 +7,6 @@ function Todo(props) {
       <li className="todo-item">{props.title}</li>
       <button className="complete-btn"><i className="fas fa-check"></i></button>
       <button className="trash-btn" onClick={()=>props.removeTodo(props.index)}><i className="fas fa-trash"></i></button>
-      
-      
     </div>
 }
 export default Todo;
