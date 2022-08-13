8bff9582acba9f7e66ea33ae002f970d581d9ad0
Flavio Ialongo <flavioialongo@gmail.com>
2022-08-13 12:38:41 +0200

    modifiche

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App(){
     const [selection, setSelection] = useState("all");
     const [filteredTodo, setFilteredTodo]=useState([])
 
-
+    const [title, setTitle] = useState("ur todo")
 
     function filter(){
         switch(selection){
@@ -24,15 +24,29 @@ function App(){
                 break;
             }
     }
+        // fetching the GET route from the Express server which matches the GET route from server.js
+      async function callBackendAPI(){
+        const response = await fetch('/express_backend');
+        const body = await response.json();
+        if (response.status !== 200) {
+          throw Error(body.message) 
+        }
+        return body;
+      };
 
- 
-    useEffect(()=>getItems(), []);
+
+
+
+    useEffect(()=>{
+        //getItems()
+        callBackendAPI().then((res)=>setTitle(res.express)).catch(err=>console.log(err))
+    }, []);
    
     useEffect(()=>{
         filter();
-        saveItems();
+        //saveItems();
     }, [todoList,selection]);
-
+ /*
     function saveItems(){
         localStorage.setItem("todos", JSON.stringify(todoList));
     }
@@ -48,11 +62,11 @@ function App(){
         
     }
 
-    
+    */
 
     return(
     <div className="App">       
-        <header>your todo</header>
+        <header>{title}</header>
         <Form   
         setSelection={setSelection}
         setList={setList} 
